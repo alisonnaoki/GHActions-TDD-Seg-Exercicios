0fb9a359ed97e78e252e2eb4a729592610d805b9
alisonnaoki <alison.tsuboi@iesb.edu.br>
2024-10-13 12:20:05 -0300

    atividade1 TDD

--- src/math.test.js
+++ src/math.test.js
@@ -22,28 +22,28 @@ describe('função fatorial', () => {
   });
 
   test('calcula fatorial de 0 para ser igual a 1', () => {
-    expect(factorial(0)).toBe(1);
+    expect(fatorial(0)).toBe(1);
   });
 });
 
 describe('função fibonacci', () => {
   test('valida entrada', () => {
     // Verifica se a função 'fibonnacci' existe
-    expect(fibonnacci).toBeDefined();
+    expect(fibonacci).toBeDefined();
     // Verifica se a função 'fibonnacci' possui 1 argumento
-    expect(fibonnacci.length).toBe(1);
+    expect(fibonacci.length).toBe(1);
 
     // Garantir que o argumento é um número
-    expect(() => fibonnacci("Texto")).toThrow(TypeError);
-    expect(() => fibonnacci("Texto")).toThrow("O argumento deve ser um número válido");
-    expect(() => fibonnacci("10")).not.toThrow(TypeError);
-    expect(() => fibonnacci(10)).not.toThrow(TypeError);
+    expect(() => fibonacci("Texto")).toThrow(TypeError);
+    expect(() => fibonacci("Texto")).toThrow("O argumento deve ser um número válido");
+    expect(() => fibonacci("10")).not.toThrow(TypeError);
+    expect(() => fibonacci(10)).not.toThrow(TypeError);
 
     // Garantir que não existe fibonnacci de número menor ou ingual a zero
-    expect(() => fibonnacci(-10)).toThrow(Error);
-    expect(() => fibonnacci(-10)).toThrow("Não existe fibonnacci de número menor ou ingual a zero");
-    expect(() => fibonnacci(0)).toThrow(Error);
-    expect(() => fibonnacci(0)).toThrow("Não existe fibonnacci de número menor ou ingual a zero");
+    expect(() => fibonacci(-10)).toThrow(Error);
+    expect(() => fibonacci(-10)).toThrow("Não existe fibonacci de número menor ou igual a zero");
+    expect(() => fibonacci(0)).toThrow(Error);
+    expect(() => fibonacci(0)).toThrow("Não existe fibonacci de número menor ou igual a zero");
   });
   test('Retorna o número fibonacci de 8 igual a 21', () => {
     expect(fibonacci(8)).toBe(21);
@@ -69,11 +69,11 @@ describe('função ehPrimo', () => {
 
     // Garantir que não existe número primo de número menor ou igual a 1
     expect(() => ehPrimo(1)).toThrow(Error);
-    expect(() => ehPrimo(1)).toThrow("Não número primo de número menor ou igual a 1");
+    expect(() => ehPrimo(1)).toThrow("Não existe número primo de número menor ou igual a 1");
     expect(() => ehPrimo(0)).toThrow(Error);
-    expect(() => ehPrimo(0)).toThrow("Não número primo de número menor ou igual a 1");
+    expect(() => ehPrimo(0)).toThrow("Não existe número primo de número menor ou igual a 1");
     expect(() => ehPrimo(-2)).toThrow(Error);
-    expect(() => ehPrimo(-2)).toThrow("Não número primo de número menor ou igual a 1");
+    expect(() => ehPrimo(-2)).toThrow("Não existe número primo de número menor ou igual a 1");
   });
   test('verifica que 7 e 11 são números primos', () => {
     expect(ehPrimo( 7)).toBe(true);
